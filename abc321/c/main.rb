c39c743e95cc746c8c4d0b9b0a8e05eef8d71358
Hayato Kawai <fohte.hk@gmail.com>
2023-09-23 22:32:06 +0900

    c: submit

--- abc321/c/main.rb
+++ abc321/c/main.rb
@@ -1,19 +1,26 @@
 K = gets.chomp.to_i
 
-def like_321?(n)
-  return true if n.length == 1
+def like_321(n)
+  ns = n.to_s
+  return [true, n + 1] if ns.length == 1
 
   (0..).each do |i|
-    return true if n[i + 1].nil?
-    return false if n[i] <= n[i + 1]
+    return [true, n + 1] if ns[i + 1].nil?
+
+    # 210 => 211 => 220 => 300
+    return [false, [*ns[0...i], ns[i].to_i + 1, '0' * (ns.size - i - 1)].join.to_i] if ns[i] <= ns[i + 1]
   end
 end
 
+i = 1
 k = 0
-(1..).each do |i|
-  k += 1 if like_321?(i.to_s)
+loop do
+  prev_i = i
+  like, i = like_321(i)
+  k += 1 if like
+
   if k == K
-    puts i.to_s
+    puts prev_i.to_s
     exit
   end
 end
