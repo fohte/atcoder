d13598fa3ed9336bc5945ece9831b771a129c979
Hayato Kawai <fohte.hk@gmail.com>
2023-09-30 21:47:44 +0900

    c: submit

--- abc322/c/main.rb
+++ abc322/c/main.rb
@@ -2,6 +2,7 @@ N, M, = gets.chomp!.split(' ').map(&:to_i)
 As = gets.chomp!.split(' ').map(&:to_i)
 
 (1..N).each do |i|
-  b = As.find { |a| a >= i }
-  puts(b - i)
+  a_i = As.find_index { |a| a >= i }
+  puts(As[a_i] - i)
+  As.shift(a_i)
 end
