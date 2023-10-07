8552bbb062315cce4ca037479296acef0e45cfdc
Hayato Kawai <fohte.hk@gmail.com>
2023-10-07 22:32:27 +0900

    abc323/c: submit

--- contests/abc323/c/main.rb
+++ contests/abc323/c/main.rb
@@ -3,18 +3,18 @@ As = gets.chomp!.split.map(&:to_i)
 Ss = N.times.map { gets.chomp! }
 
 scores = Ss.map.with_index do |s, i|
-  s.chars.map.with_index { |v, j| [v == 'o' ? As[j] + i + 1 : 0, j] }
+  s.chars.map { |v, j| v == 'o' ? As[j] + i + 1 : 0 }
 end
 
 SORTED_SS = scores.map.with_index.sort_by { |s, _| s }.reverse
 
 def calc(me, other, i)
-  other_score = other.map { |v, _| v }.sum
+  other_score = other.sum
   count = 0
-  new_score = me.map { |v, _| v }.sum
+  new_score = me.sum
 
   SORTED_SS.each do |_s, j|
-    next if me[j][0] != 0
+    next if me[j] != 0
 
     return count if other_score < new_score
 
