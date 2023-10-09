0e48bf105d835435e6e14971c3566d15b55628ae
Hayato Kawai <fohte.hk@gmail.com>
2023-10-09 22:29:05 +0900

    abc065/abc065_b: submit 2023-10-09T22:29:05+09:00

---
+++ contests/abc065/b/generate_in.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+
+N = 10
+puts N
+
+N.times do
+  puts rand(1..N)
+end
--- contests/abc065/b/main.rb
+++ contests/abc065/b/main.rb
@@ -2,10 +2,6 @@ N = gets.to_i
 A = {}.tap do |h|
   N.times.each do |i|
     h[i + 1] = gets.to_i
-    if h[i + 1] == i + 1
-      puts '-1'
-      exit
-    end
   end
 end
 
