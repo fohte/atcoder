b31e548fda48d250e534d64526383bf7396be6bc
Hayato Kawai <fohte.hk@gmail.com>
2023-10-09 22:31:08 +0900

    practice (2023-10-08): AtCoder Problems Training (#13)

---
+++ contests/abc065/b/main.rb
@@ -0,0 +1,24 @@
+N = gets.to_i
+A = {}.tap do |h|
+  N.times.each do |i|
+    h[i + 1] = gets.to_i
+  end
+end
+
+button = 1
+count = 0
+
+loop do
+  if button == 2
+    puts count
+    exit
+  end
+  button = A.delete(button)
+  if button.nil?
+    puts '-1'
+    exit
+  end
+  count += 1
+end
+
+puts '-1'
