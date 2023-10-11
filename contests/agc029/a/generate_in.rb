91c16846dbe96074a7f1682154fe167e5f853c59
Hayato Kawai <fohte.hk@gmail.com>
2023-10-11 10:16:04 +0900

    agc029/agc029_a: submit 2023-10-11T10:16:03+09:00

---
+++ contests/agc029/a/generate_in.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+(2 * 10**3).times do
+  print(rand(2) == 1 ? 'B' : 'W')
+end
+
+puts
--- contests/agc029/a/main.rb
+++ contests/agc029/a/main.rb
@@ -12,7 +12,11 @@ while i < S.length
   if left == 'B' && right == 'W'
     S[i] = 'W'
     S[i + 1] = 'B'
-    i = 0
+    if i > 0
+      i -= 1
+    else
+      i = 0
+    end
     count += 1
   else
     i += 1
