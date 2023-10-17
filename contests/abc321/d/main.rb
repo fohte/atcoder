57f11775907ba2daef532aac562d2c1bac9b9158
Hayato Kawai <fohte.hk@gmail.com>
2023-10-17 22:55:05 +0900

    practice (2023-10-17): ABC321 retry (#20)

--- contests/abc321/d/main.rb
+++ contests/abc321/d/main.rb
@@ -1,12 +1,15 @@
 N, M, P, = gets.split.map(&:to_i)
 As = gets.split.map(&:to_i)
-Bs = gets.split.map(&:to_i)
+Bs = gets.split.map(&:to_i).sort
 
+b_prefix_sum = (0...M).each_with_object([0]) do |i, acc|
+  acc << (acc.last + Bs[i])
+end
 r = 0
 As.each do |a|
-  Bs.each do |b|
-    r += [a + b, P].min
-  end
+  n = Bs.bsearch_index { |b| b >= P - a } || M
+
+  r += a * n + b_prefix_sum[n] + P * (M - n)
 end
 
 puts r
