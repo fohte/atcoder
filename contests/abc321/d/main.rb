2406d8e2abd6d178fbb4dbbdda0c57a8a51187c4
Hayato Kawai <fohte.hk@gmail.com>
2023-10-17 22:03:37 +0900

    abc321/abc321_d: submit 2023-10-17T22:03:37+09:00

--- contests/abc321/d/main.rb
+++ contests/abc321/d/main.rb
@@ -1,9 +1,14 @@
 N, M, P, = gets.split.map(&:to_i)
-As = gets.split.map(&:to_i).delete_if { |a| a > P }
-Bs = gets.split.map(&:to_i).delete_if { |b| b > P }
+As = gets.split.map(&:to_i)
+Bs = gets.split.map(&:to_i)
 
 r = 0
 As.each do |a|
+  if a >= P
+    r += P * M
+    next
+  end
+
   Bs.each do |b|
     r += [a + b, P].min
   end
