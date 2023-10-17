56f582ba052d5ab5d59a4ef45bdc69e7a16129f5
Hayato Kawai <fohte.hk@gmail.com>
2023-10-17 22:47:29 +0900

    abc321/abc321_d: submit 2023-10-17T22:47:29+09:00

--- contests/abc321/d/main.rb
+++ contests/abc321/d/main.rb
@@ -3,11 +3,11 @@ As = gets.split.map(&:to_i)
 Bs = gets.split.map(&:to_i).sort
 
 b_prefix_sum = (0...M).each_with_object([0]) do |i, acc|
-  acc << ((acc.last || 0) + Bs[i])
+  acc << (acc.last + Bs[i])
 end
 r = 0
 As.each do |a|
-  n = Bs.bsearch_index { |b| b >= P - a }
+  n = Bs.bsearch_index { |b| b >= P - a } || M
 
   r += a * n + b_prefix_sum[n] + P * (M - n)
 end
