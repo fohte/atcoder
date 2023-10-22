fdc66699ea0882be9d4e77a951c26cfb4b87a9f7
Hayato Kawai <fohte.hk@gmail.com>
2023-10-22 12:50:51 +0900

    tessoku-book/math_and_algorithm_al: submit 2023-10-22T12:50:51+09:00

---
+++ contests/tessoku-book/b07/main.rb
@@ -0,0 +1,13 @@
+T = gets.to_i
+N = gets.to_i
+
+SD = Array.new(T, 0)
+N.times do
+  l, r = gets.split.map(&:to_i)
+  SD[l] += 1
+  SD[r] -= 1
+end
+
+S = SD.each_with_object([0]) { |x, a| a << a.last + x }
+
+puts S[1..]
