9df42242deea99eeb34ff1e33d2bcb72f62200d5
Hayato Kawai <fohte.hk@gmail.com>
2023-11-07 22:44:50 +0900

    tessoku-book/tessoku_book_i: submit 2023-11-07T22:44:50+09:00

--- contests/tessoku-book/a09/main.rb
+++ contests/tessoku-book/a09/main.rb
@@ -4,6 +4,11 @@ T = Array.new(H + 1) { Array.new(W + 1, 0) }
 S = Array.new(H + 1) { Array.new(W + 1, 0) }
 
 ABCD.each do |a, b, c, d|
+  a -= 1
+  b -= 1
+  c -= 1
+  d -= 1
+
   T[a][b] += 1
   T[a][d + 1] -= 1
   T[c + 1][b] -= 1
@@ -12,7 +17,7 @@ end
 
 (1..H).each do |y|
   (1..W).each do |x|
-    S[y][x] = S[y][x - 1] + T[y][x]
+    S[y][x] = S[y][x - 1] + T[y - 1][x - 1]
   end
 end
 
