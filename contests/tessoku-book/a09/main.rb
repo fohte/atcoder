c635ee147c3fb5231a190e6c7c9b7c480d9b57c5
Hayato Kawai <fohte.hk@gmail.com>
2023-11-07 22:43:28 +0900

    tessoku-book/tessoku_book_i: submit 2023-11-07T22:43:28+09:00

---
+++ contests/tessoku-book/a09/main.rb
@@ -0,0 +1,25 @@
+H, W, N = gets.split.map(&:to_i)
+ABCD = Array.new(N) { gets.split.map(&:to_i) }
+T = Array.new(H + 1) { Array.new(W + 1, 0) }
+S = Array.new(H + 1) { Array.new(W + 1, 0) }
+
+ABCD.each do |a, b, c, d|
+  T[a][b] += 1
+  T[a][d + 1] -= 1
+  T[c + 1][b] -= 1
+  T[c + 1][d + 1] += 1
+end
+
+(1..H).each do |y|
+  (1..W).each do |x|
+    S[y][x] = S[y][x - 1] + T[y][x]
+  end
+end
+
+(1..W).each do |x|
+  (1..H).each do |y|
+    S[y][x] += S[y - 1][x]
+  end
+end
+
+puts S[1..].map { |s| s[1..].join(' ') }.join("\n")
