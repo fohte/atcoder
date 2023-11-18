3cdebb15c33d534061167f55757bf776d3d6c045
Hayato Kawai <fohte.hk@gmail.com>
2023-11-18 21:39:28 +0900

    abc329/abc329_d: submit 2023-11-18T21:39:28+09:00

--- contests/abc329/d/main.rb
+++ contests/abc329/d/main.rb
@@ -1,2 +1,14 @@
 N, M = gets.split.map(&:to_i)
 A = gets.split.map(&:to_i)
+
+C = Hash.new(0)
+
+def find_keys(hash, value)
+  hash.select { |k, v| v == value }.keys
+end
+
+M.times do |i|
+  a = A[i]
+  C[a] += 1
+  puts find_keys(C, C.values.max).min
+end
